e299ea6f5dc21c1c08d92a81bfd2094819933f23
Thomas Volk (MBP-von-Thomas.fritz.box) <info@thomasvolk.de>
2026-06-18 22:09:48 +0200

    add fuer elise

---
+++ examples/fur_elise.py
@@ -0,0 +1,33 @@
+from propeller.notes import *
+from propeller import project, track
+
+# Beethoven - Für Elise (WoO 59), main theme
+# 3/4 time, 1 beat = quarter note; eighth notes use *0.5
+p = project(
+    bpm=100,
+    time_signature=(3, 4),
+    bars=8,
+    tracks=[
+        track(
+            name="Piano",
+            channel=1,
+            instrument=0,
+            notes=[
+                # Opening motif
+                E5*0.5, Ef5*0.5, E5*0.5, Ef5*0.5, E5*0.5, B4*0.5,
+                D5*0.5, C5*0.5, A4*1.0,
+                # First interlude
+                Z*0.5, C4*0.5, E4*0.5, A4*1.0, B4*0.5,
+                Z*0.5, E4*0.5, Gs4*0.5, B4*1.0, C5*0.5,
+                # Repeat opening motif
+                Z*0.5, E5*0.5, Ef5*0.5, E5*0.5, Ef5*0.5, E5*0.5,
+                B4*0.5, D5*0.5, C5*0.5, A4*1.0,
+                # Modified ending
+                Z*0.5, C4*0.5, E4*0.5, A4*1.0, B4*0.5,
+                Z*0.5, E4*0.5, C5*0.5, B4*0.5, A4*1.5,
+                Z*1.5,
+            ],
+        ),
+    ],
+)
+p.play()
