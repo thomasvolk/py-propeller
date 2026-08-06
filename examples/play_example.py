e8070f506a125feef5a3b9042413a6c0b6201107
Thomas Volk (MacBook) <info@thomasvolk.de>
2026-08-06 23:02:57 +0200

    improve play example

--- examples/play_example.py
+++ examples/play_example.py
@@ -1,19 +1,36 @@
-from propeller.notes import *
+from propeller.notes import C4, D4, E4, F4, G4, A4, B4, C5, D5, E5, F5, G5, A5, B5
 from propeller import project, track
 
 p = project(
     bpm=120,
-    time_signature=(4, 4),
-    bars=1,
+    time_signature=(8, 8),
+    bars=2,
     tracks=[
         track(name="Piano",
               channel=2,
               instrument=0,
               notes=[
-                  C4(120),
-                  D4(100),
-                  E4(100),
-                  F4]
+              [
+                  C4,
+                  C5,
+                  E4,
+                  E5,
+                  D4,
+                  D5,
+                  E4,
+                  E5,
+
+                  C4,
+                  C5,
+                  E4,
+                  E5,
+                  D4,
+                  D5,
+                  C4,
+                  C5,
+
+              ],
+            ]
         ),
     ],
 )
