6d40df342371ca3ed6d191fa63ebc7997c734673
Thomas Volk (MBP-von-Thomas.fritz.box) <info@thomasvolk.de>
2026-08-08 09:52:29 +0200

    improve play example

--- examples/play_example.py
+++ examples/play_example.py
@@ -1,16 +1,19 @@
-from propeller.notes import C4, D4, E4, F4, G4, A4, B4, C5, D5, E5, F5, G5, A5, B5
+from propeller.notes import C4, D4, E4, F4, G4, A4, B4, C5, D5, E5, F5, G5, A5, B5, G6, F6, PB
 from propeller import project, track
+
+# Surge XT - Slowboat -> Basses -> Bass FX 1
 
 p = project(
     bpm=120,
     time_signature=(8, 8),
     bars=2,
     tracks=[
-        track(name="Piano",
-              channel=2,
+        track(name="Bass",
+              channel=4,
               instrument=0,
               notes=[
                 [
+                  PB(0),
                   C4,
                   C5,
                   E4,
@@ -28,6 +31,18 @@ p = project(
                   D5,
                   C4,
                   C5,
+
+              ],
+            ]
+        ),
+        track(name="Lead",
+              channel=2,
+              instrument=0,
+              notes=[
+                [
+                  G6 * 8,
+
+                  F6 * 8,
 
               ],
             ]
