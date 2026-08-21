7e1084c8088d6781b52b2853b55d63af9ba8eddc
Thomas Volk (MacBook) <info@thomasvolk.de>
2026-08-21 21:28:31 +0200

    use position for variation

--- examples/beats_and_bass_example.py
+++ examples/beats_and_bass_example.py
@@ -1,10 +1,24 @@
 from propeller.notes import C4, D4, E4, F2, D3, C3, Z
 from propeller import project, track
+from propeller import loop
 
+p = loop.get_position()
+
+# Note definitions for Korg Volca Drum
 BD = C4
 HH = D4
 SN = E4
 R = Z
+
+if p.loop_count % 2 == 0:
+    hh = []
+else:
+    hh = [
+           HH * 0.5, HH * 0.5,
+           HH * 0.5, HH * 0.5,
+           HH * 0.5, HH * 0.5,
+           HH * 0.5, HH * 0.5,
+          ] * 2
 
 p = project(
     bpm=100,
@@ -26,12 +40,7 @@ p = project(
                   BD *  0.5,
                   BD *  1,
               ],
-              [
-               HH * 0.5, HH * 0.5,
-               HH * 0.5, HH * 0.5,
-               HH * 0.5, HH * 0.5,
-               HH * 0.5, HH * 0.5,
-              ] * 2
+              hh
         ]),
         track(name="Bass",
               channel=4,
